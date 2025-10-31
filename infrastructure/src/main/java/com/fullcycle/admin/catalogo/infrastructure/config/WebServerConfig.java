bf8ef14baf69223b8f8bb70bbc4586e5a5739070
daniel-castilho <dan.castilho@gmail.com>
2025-10-31 15:23:18 -0400

    feat(infra): Configura a inicialização e o servidor web da aplicação Spring Boot
    Este commit introduz as configurações essenciais para que a aplicação Spring Boot seja inicializada e executada corretamente.
    
    - **Classe Principal (`Main.java`):** A classe `Main` foi devidamente anotada com `@SpringBootApplication` e seu método `main` foi ajustado para usar `SpringApplication.run()`, estabelecendo o ponto de entrada padrão do Spring Boot.
    - **Configuração do Servidor Web (`WebServerConfig.java`):** Foi criada a classe `WebServerConfig` para configurar o servidor web, garantindo que a aplicação suba corretamente.
    - **Ajuste de Build (`build.gradle`):** O conflito de plugins no `build.gradle` foi resolvido, um passo crítico que desbloqueou a execução do build e, consequentemente, da aplicação.
    
    Essas alterações, em conjunto, garantem que a aplicação Spring Boot agora pode ser iniciada com sucesso e está pronta para servir as funcionalidades da API.

---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/config/WebServerConfig.java
@@ -0,0 +1,8 @@
+package com.fullcycle.admin.catalogo.infrastructure.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.fullcycle.admin.catalogo")
+public class WebServerConfig {}
