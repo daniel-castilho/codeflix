1cbdf95ad6834b76369232f900c7ea10e360e6b2
daniel-castilho <dan.castilho@gmail.com>
2025-10-31 10:54:41 -0400

    feat(category): Adiciona o DeleteCategoryOutput
    Cria o Data Transfer Object (DTO) `DeleteCategoryOutput`, que servirá como o objeto de saída para uma execução bem-sucedida do `DeleteCategoryUseCase`.
    
    A classe foi implementada como um `record` do Java e inclui um método `from()` para facilitar a sua instanciação.

--- application/src/main/java/com/fullcycle/admin/catalogo/application/category/delete/DeleteCategoryOutput.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/category/delete/DeleteCategoryOutput.java
@@ -1,4 +1,10 @@
 package com.fullcycle.admin.catalogo.application.category.delete;
 
-public record DeleteCategoryOutput() {
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+
+public record DeleteCategoryOutput(CategoryID id) {
+
+    public static DeleteCategoryOutput from(final CategoryID aId) {
+        return new DeleteCategoryOutput(aId);
+    }
 }
